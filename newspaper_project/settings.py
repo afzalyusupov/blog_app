490a4c059b4b4cf7c9cc60a39e8af60956081a0c
Afzal Yusupov <afzal-yusupov@mail.ru>
2021-02-10 19:16:41 +0500

    Update settings.py

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -22,7 +22,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p@-47!5x7sl+zu)2ob@!2sizg1xm5hsu)$w##ov&b$-7zhy@we'
+SECRET_KEY = ' '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
